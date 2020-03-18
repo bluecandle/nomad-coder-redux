ce30b5515f29024723c392894c5bd805f2011547
bluecandle <jinyeongkim37@gmail.com>
2020-03-18 23:29:22 +0900

    done until #2.4 (skipped chapter 2).

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Vanilla JS 를 이용한 counter 진행 : html 에게 뭔가가 바뀌었다고 알려주기 위해 함수를 사용해야 한다는 것이 redux
+// 가 필요해지는 지점!
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const add = document.getElementById("minus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+    console.log(count, action);
+    switch (action.type) {
+        case ADD:
+          return count + 1;
+        case MINUS:
+          return count - 1;
+        default:
+          return count;
+    }
+};
+const countStore = createStore(countModifier);
+
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({type: ADD});
+};
+
+const handleMinus = () => {
+    countStore.dispatch({type: MINUS});
+};
+
+console.log(countStore.getState());
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
